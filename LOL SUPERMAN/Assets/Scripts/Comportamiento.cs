18388ad00dfb4c55835e03b178ed3714ff0c630f
shinjekyag <shinjekyag@gmail.com>
2026-09-10 15:15:43 -0600

    aorasi
    comitiado

---
+++ LOL SUPERMAN/Assets/Scripts/Comportamiento.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Comportamiento : MonoBehaviour
+{
+    public int numero;
+    [SerializeField] private int queso;
+    int refresco;
+
+    public void Calentar()
+    {
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int pan;
+        pan = 2;
+        numero = 0;
+        Debug Log
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
